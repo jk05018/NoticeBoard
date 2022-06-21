a105af64a9cb310256b6bed2ea7bfc8816ba65a7
SeungHan <jk05018@naver.com>
2022-06-21 16:58:39 +0900

    fix : service test 수정

--- src/test/java/com/example/notice/domain/notice/service/NoticeServiceTest.java
+++ src/test/java/com/example/notice/domain/notice/service/NoticeServiceTest.java
@@ -1,20 +1,13 @@
 package com.example.notice.domain.notice.service;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
-import java.util.stream.IntStream;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.notice.domain.BasicServiceTest;
 import com.example.notice.domain.notice.entity.Body;
 import com.example.notice.domain.notice.entity.Notice;
 import com.example.notice.domain.notice.entity.Slug;
 import com.example.notice.domain.notice.entity.Title;
-import com.example.notice.domain.notice.repository.NoticeRepository;
 import com.example.notice.domain.user.entity.Age;
 import com.example.notice.domain.user.entity.Email;
 import com.example.notice.domain.user.entity.NickName;
@@ -24,129 +17,133 @@ import com.example.notice.domain.user.entity.User;
 import com.example.notice.domain.user.entity.Username;
 import com.example.notice.exception.NoSuchNoticeException;
 import com.example.notice.exception.NoticeProjectException;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class NoticeServiceTest extends BasicServiceTest {
 
-	@Autowired
-	private NoticeService noticeService;
+  @Autowired
+  private NoticeService noticeService;
 
-	@Autowired
-	private NoticeRepository noticeRepository;
+  @Test
+  void 공지사항을_ID로_조회할_수_있다() {
+    // given
+    final Title title = new Title("title");
+    final Body body = new Body("body");
+    final Slug slug = new Slug(title);
 
-	@Test
-	void 공지사항을_ID로_조회할_수_있다() {
-		// given
-		final Title title = new Title("title");
-		final Body body = new Body("body");
-		final Slug slug = new Slug(title);
+    final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
 
-		final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
+    // when
+    final Notice findNotice = noticeService.getNoticeBySlug(slug);
 
-		// when
-		final Notice findNotice = noticeService.getNoticeBySlug(slug);
+    // then
+    assertThat(findNotice).extracting(Notice::getTitle, Notice::getSlug, Notice::getBody,
+            Notice::getWriter)
+        .isEqualTo(List.of(notice.getTitle(), notice.getSlug(), notice.getBody(), testProfile));
 
-		// then
-		assertThat(findNotice).extracting(Notice::getTitle, Notice::getSlug, Notice::getBody, Notice::getWriter)
-			.isEqualTo(List.of(notice.getTitle(), notice.getSlug(), notice.getBody(), testProfile));
+  }
 
-	}
+  @Test
+  void 공지사항_전체_목록을_조회할수_있다() {
+    // given
+    final Notice notice1 = 공지사항_등록(new Title("title1"), new Body("body1"), testUser, testProfile);
+    final Notice notice2 = 공지사항_등록(new Title("title2"), new Body("body2"), testUser, testProfile);
+    final Notice notice3 = 공지사항_등록(new Title("title3"), new Body("body3"), testUser, testProfile);
 
-	@Test
-	void 공지사항_전체_목록을_조회할수_있다() {
-		// given
-		final Notice notice1 = 공지사항_등록(new Title("title1"), new Body("body1"), testUser, testProfile);
-		final Notice notice2 = 공지사항_등록(new Title("title2"), new Body("body2"), testUser, testProfile);
-		final Notice notice3 = 공지사항_등록(new Title("title3"), new Body("body3"), testUser, testProfile);
+    // when
+    final List<Notice> noticeList = noticeService.getNoticeList();
 
-		// when
-		final List<Notice> noticeList = noticeService.getNoticeList();
+    // then
+    assertThat(noticeList).containsExactlyInAnyOrder(notice1, notice2, notice3);
+  }
 
-		// then
-		assertThat(noticeList).containsExactlyInAnyOrder(notice1, notice2, notice3);
-	}
+  @Test
+  void 공지사항의_제목과_내용을_수정할_수_있다() {
+    // given
+    final Title title = new Title("title");
+    final Body body = new Body("body");
 
-	@Test
-	void 공지사항의_제목과_내용을_수정할_수_있다() {
-		// given
-		final Title title = new Title("title");
-		final Body body = new Body("body");
+    final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
 
-		final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
+    // when
+    final Title updateTitle = new Title("update title");
+    final Body updateBody = new Body("update Body");
 
-		// when
-		final Title updateTitle = new Title("update title");
-		final Body updateBody = new Body("update Body");
-		noticeService.updateNotice(Notice.updateOf(updateTitle, updateBody), new Slug(title));
+    noticeService.updateNotice(Notice.updateOf(updateTitle, updateBody), new Slug(title));
 
-		// then
-		final Notice updatedNotice = noticeService.getNoticeBySlug(new Slug(title));
+    // then
+    final Notice updatedNotice = noticeService.getNoticeBySlug(new Slug(title));
 
-		assertThat(updatedNotice).extracting(Notice::getTitle, Notice::getBody)
-			.isEqualTo(List.of(updateTitle, updateBody));
+    assertThat(updatedNotice).extracting(Notice::getTitle, Notice::getBody)
+        .isEqualTo(List.of(updateTitle, updateBody));
 
-	}
+  }
 
-	@Test
-	void 공지사항을_ID로_삭제할수_있다() {
-		// given
-		final Title title = new Title("title");
-		final Body body = new Body("body");
-		final Slug slug = new Slug(title);
+  @Test
+  void 공지사항을_ID로_삭제할수_있다() {
+    // given
+    final Title title = new Title("title");
+    final Body body = new Body("body");
+    final Slug slug = new Slug(title);
 
-		final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
+    final Notice notice = 공지사항_등록(title, body, testUser, testProfile);
 
-		// when
-		noticeService.deleteNoticeBySlug(slug);
+    // when
+    noticeService.deleteNoticeBySlug(slug);
 
-		// then
-		assertThrows(NoSuchNoticeException.class, () -> noticeService.getNoticeBySlug(slug));
+    // then
+    assertThrows(NoSuchNoticeException.class, () -> noticeService.getNoticeBySlug(slug));
 
-	}
+  }
 
-	@Test
-	void 사용자가_작성한_공지사항을_조회할수_있다() {
-		// given
-		공지사항_랜덤_등록(10, "title", "body");
+  @Test
+  void 사용자가_작성한_공지사항을_조회할수_있다() {
+    // given
+    공지사항_랜덤_등록(10, "title", "body");
 
-		// when
-		final List<Notice> noticeListOfProfile = noticeService.getNoticeListByUsername(testUser.getUsername());
+    // when
+    final List<Notice> noticeListOfProfile = noticeService.getNoticeListByUsername(
+        testUser.getUsername());
 
-		// then
-		assertThat(noticeListOfProfile.size()).isEqualTo(5);
-		assertThat(noticeListOfProfile).extracting(Notice::getWriter)
-			.allSatisfy(writer -> writer.equals(testProfile));
-	}
+    // then
+    assertThat(noticeListOfProfile.size()).isEqualTo(5);
+    assertThat(noticeListOfProfile).extracting(Notice::getWriter)
+        .allSatisfy(writer -> writer.equals(testProfile));
+  }
 
-	Notice 공지사항_등록(final Title title, final Body body, final User user, final Profile profile) {
-		try {
-			final Notice notice = Notice.createOf(title, body);
+  Notice 공지사항_등록(final Title title, final Body body, final User user, final Profile profile) {
+    try {
+      final Notice notice = Notice.createOf(title, body);
 
-			final Notice savedNotice = noticeService.createNotice(notice, user.getUsername());
+      final Notice savedNotice = noticeService.createNotice(notice, user.getUsername());
 
-			assertThat(savedNotice).extracting(Notice::getTitle, Notice::getBody, Notice::getWriter)
-				.isEqualTo(List.of(title, body, profile));
+      assertThat(savedNotice).extracting(Notice::getTitle, Notice::getBody, Notice::getWriter)
+          .isEqualTo(List.of(title, body, profile));
 
-			return savedNotice;
-		} catch (Exception e) {
-			throw new NoticeProjectException("테스트 공지사항 등록 실패 입니다.", e);
-		}
-	}
+      return savedNotice;
+    } catch (Exception e) {
+      throw new NoticeProjectException("테스트 공지사항 등록 실패 입니다.", e);
+    }
+  }
 
-	void 공지사항_랜덤_등록(final int noticecount, final String title, final String body) {
-		try {
-			final User testUser2 = 유저_등록(new Username("seunghan2"), new Email("seunghan2@naver.com"),
-				new Password("pass123"));
-			final Profile testProfile2 = 프로필_등록(testUser2, new NickName("testNickname2"), new Age(23));
+  void 공지사항_랜덤_등록(final int noticecount, final String title, final String body) {
+    try {
+      final User testUser2 = 유저_등록(new Username("seunghan2"), new Email("seunghan2@naver.com"),
+          new Password("pass123"));
+      final Profile testProfile2 = 프로필_등록(testUser2, new NickName("testNickname2"), new Age(23));
 
-			IntStream.range(0, noticecount)
-				.forEach(count ->
-					공지사항_등록(new Title(title + count), new Body(body + count),
-						(count % 2) == 0 ? testUser : testUser2,
-						(count % 2 == 0) ? testProfile : testProfile2)
-				);
-		} catch (Exception e) {
-			throw new NoticeProjectException("테스트 공지사항 랜덤 등록 실패 입니다.", e);
-		}
-	}
+      IntStream.range(0, noticecount)
+          .forEach(count ->
+              공지사항_등록(new Title(title + count), new Body(body + count),
+                  (count % 2) == 0 ? testUser : testUser2,
+                  (count % 2 == 0) ? testProfile : testProfile2)
+          );
+    } catch (Exception e) {
+      throw new NoticeProjectException("테스트 공지사항 랜덤 등록 실패 입니다.", e);
+    }
+  }
 
 }
--- src/test/java/com/example/notice/domain/user/service/UserServiceTest.java
+++ src/test/java/com/example/notice/domain/user/service/UserServiceTest.java
@@ -1,12 +1,6 @@
 package com.example.notice.domain.user.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.notice.domain.BasicServiceTest;
 import com.example.notice.domain.user.entity.Age;
@@ -16,70 +10,69 @@ import com.example.notice.domain.user.entity.Password;
 import com.example.notice.domain.user.entity.Profile;
 import com.example.notice.domain.user.entity.User;
 import com.example.notice.domain.user.entity.Username;
-import com.example.notice.domain.user.repository.UserRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class UserServiceTest extends BasicServiceTest {
 
-	@Autowired
-	private UserService userService;
+  @Autowired
+  private UserService userService;
 
-	@Autowired
-	private UserRepository userRepository;
+  @Test
+  void 유저를_이름으로_조회할수_있다() {
+    // given
+    final Username username = new Username("test");
+    final Email email = new Email("test@naver.com");
+    final Password password = new Password("password123");
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    유저_등록(username, email, password);
 
-	@Test
-	void 유저를_이름으로_조회할수_있다() {
-		// given
-		final Username username = new Username("test");
-		final Email email = new Email("test@naver.com");
-		final Password password = new Password("password123");
+    // when
+    final User findUser = userService.findByUsername(username);
 
-		유저_등록(username, email, password);
-		// when
-		final User findUser = userService.findByUsername(username);
+    // then
+    assertThat(findUser.getUsername()).isEqualTo(username);
+    assertThat(findUser.getPassword()).isNotNull();
+  }
 
-		// then
-		assertThat(findUser.getUsername()).isEqualTo(username);
-		assertThat(findUser.getPassword()).isNotNull();
-	}
+  @Test
+  void 아이디와_비밀번호로_로그인할수_있다() {
+    // given
+    final Username username = new Username("test");
+    final Email email = new Email("test@naver.com");
+    final String pass = "password123";
+    final Password password = new Password(pass);
 
-	@Test
-	void 아이디와_비밀번호로_로그인할수_있다() {
-		// given
-		final Username username = new Username("test");
-		final Email email = new Email("test@naver.com");
-		final String pass = "password123";
-		final Password password = new Password(pass);
+    유저_등록(username, email, password);
 
-		유저_등록(username, email, password);
+    // when
+    final User loginUser = userService.login(username, new Password(pass));
 
-		// when
-		final User loginUser = userService.login(username, new Password(pass));
+    // then
+    assertThat(loginUser).isNotNull();
+  }
 
-		assertThat(loginUser).isNotNull();
-	}
+  @Test
+  void 프로필을_등록할수_있다() {
+    // given
+    final Username username = new Username("test");
+    유저_등록(username, new Email("test@naber.com"), new Password("pass123"));
 
-	@Test
-	void 프로필을_등록할수_있다() {
-		// given
-		final Username username = new Username("test");
-		유저_등록(username, new Email("test@naber.com"), new Password("pass123"));
+    // when
+    final NickName nickName = new NickName("testNickName");
+    final Age age = new Age(25);
 
-		// when
-		final NickName nickName = new NickName("testNickName");
-		final Age age = new Age(25);
+    final Profile profile = Profile.of(nickName, age);
 
-		final Profile profile = Profile.of(nickName, age);
-		userService.makeProfile(username, profile);
+    userService.makeProfile(username, profile);
 
-		//then
-		final User findUser = userService.findByUsername(username);
+    //then
+    final User findUser = userService.findByUsername(username);
 
-		assertThat(findUser.getProfile()).isEqualTo(profile);
-		assertThat(findUser.getProfile()).extracting(Profile::getNickName, Profile::getAge)
-			.isEqualTo(List.of(nickName, age));
-	}
+    assertThat(findUser.getProfile()).isEqualTo(profile);
+    assertThat(findUser.getProfile()).extracting(Profile::getNickName, Profile::getAge)
+        .isEqualTo(List.of(nickName, age));
+  }
 
 }
